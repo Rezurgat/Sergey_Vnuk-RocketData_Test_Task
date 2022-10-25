2fe2778fe9ca7a9c40cbd639d5ddc8e9ddcd22de
Rezurgat <Killa.S.V.ru@bk.ru>
2022-10-25 20:38:30 +0300

    Continue refactoring natura_siberica

--- Natura_Siberica/my.py
+++ Natura_Siberica/my.py
@@ -11,8 +11,8 @@ headers = {
     'user-agent': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
 }
 
-req = requests.get(url=url, headers=headers)
-src = req.text
+# req = requests.get(url=url, headers=headers)
+# src = req.text
 
 # with open('pages/natura_siberica.html', 'w', encoding='utf-8') as file:
 #     file.write(src)
@@ -23,10 +23,26 @@ with open('pages/natura_siberica.html', encoding='utf-8') as file:
 soup = BeautifulSoup(src, 'lxml')
 
 all_pages_dict = {}
-all_pages = soup.find_all('a', class_='card-list__link')
-print(all_pages)
-for link in all_pages:
-    link_name = soup.find('p', class_='card-list__name')
-    print(link_name)
-    # item_href = 'https://naturasiberica.ru' + item.get('href')
-    # all_pages_dict[item_text] = [item_href]
+pages_link = soup.find_all('a', class_='card-list__link')
+count = 0
+for link in pages_link:
+    link_name = f'natura_siberica_{count}'
+    link_href = 'https://naturasiberica.ru' + link.get('href')
+    all_pages_dict[link_name] = [link_href]
+    count += 1
+
+
+# with open('pages/all_pages.json', 'w', encoding='utf-8') as file:
+#     json.dump(all_pages_dict, file, indent=4, ensure_ascii=False)
+
+with open('pages/all_pages.json', encoding='utf-8') as file:
+    all_pages_links = json.load(file)
+
+data_list = []
+for shop_name, shop_href in all_pages_links.items():
+
+    pages = requests.get(url=shop_href[0], headers=headers)
+    head_items = Selector(text=pages.text).xpath('//p[@class="card-list__description"]/text()').extract()
+    print(head_items)
+
+
